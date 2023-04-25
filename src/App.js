8137dcbb8177e9b86c8c1a60d94b330a80a43fb7
akinGC <akarshgc1998@gmail.com>
2023-04-25 16:02:23 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@
 import Form from './Form';
 
 function App() {
+
+  async function formHandle(e){
+    e.preventDefault()
+    
+   let img = document.getElementsByName('file')[0].value
+let formfile = new FormData()
+formfile.append('file',img)
+console.log(formfile)
+  }
   return (
     <div>
-      <Form/>
+      <h1>fille</h1>
+      <form onSubmit={formHandle}>
+        <input type='file'name='file'></input>
+        <br/>
+        <input type='text' name='key'></input>
+        <br/>
+        <button type='submit'>Send</button>
+      </form>
     </div>
   );
 }
